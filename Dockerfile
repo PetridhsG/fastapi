f10c498bce01817bcae84a26d8964c8d10231f4e
petridhsg <giannispetridakos@gmail.com>
2025-12-12 21:00:53 +0200

    Initialize project files

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.13-slim-bookworm
+
+# Install uv
+COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
+
+# Create workdir
+WORKDIR /app
+
+# Copy only project files needed to install dependencies
+COPY pyproject.toml uv.lock ./
+
+# Install dependencies
+RUN uv sync --frozen
+
+# Now copy the rest of the app
+COPY . .
+
+EXPOSE 8000
+
+CMD ["uv", "run", "uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
---
+++ app/db/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from config import settings
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{settings.DATABASE_USERNAME}:"
+    f"{settings.DATABASE_PASSWORD}@"
+    f"{settings.DATABASE_HOSTNAME}:"
+    f"{settings.DATABASE_PORT}/"
+    f"{settings.DATABASE_NAME}"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
